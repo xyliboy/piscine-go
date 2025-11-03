c0b83394a0e3e7f243a785a60ffefc969dc094cc
gxylinas <woodengeorgio@gmail.com>
2025-11-03 18:06:34 +0200

    myfamily.sh

--- myfamily.sh
+++ myfamily.sh
@@ -1,4 +1,4 @@
-[ -z "$HERO_ID" ] && exit 0
+HERO_ID="${HERO_ID:-1}"
 
 curl -s https://platform.zone01.gr/assets/superhero/all.json \
 | jq -r --argjson id "$HERO_ID" '.[] | select(.id == $id) | .relatives // empty'
