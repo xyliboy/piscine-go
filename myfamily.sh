a82020eaa29e890463d14d50f563bc4e90fc3d28
gxylinas <woodengeorgio@gmail.com>
2025-11-03 18:03:07 +0200

    myfamily.sh

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,4 @@
-#curl -s https://platform.zone01.gr/assets/superhero/all.json | jq -r --arg id "$HERO_ID" '.[] | select(.id == ($id | tonumber)) | .relatives' | sed 's/; Jackie Shorr.*$//'
-curl -s https://platform.zone01.gr/assets/superhero/all.json | jq
+[ -z "$HERO_ID" ] && exit 0
+
+curl -s https://platform.zone01.gr/assets/superhero/all.json \
+| jq -r --argjson id "$HERO_ID" '.[] | select(.id == $id) | .relatives // empty'
