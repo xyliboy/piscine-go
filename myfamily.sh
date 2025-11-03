811fe3d38c131d08b9c2ae5329d155cf9345c3a5
gxylinas <woodengeorgio@gmail.com>
2025-11-03 17:38:48 +0200

    myfamily.sh

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,3 @@
 curl -s https://platform.zone01.gr/assets/superhero/all.json \
-| jq -r --arg id "$HERO_ID" '.[] | select(.id == ($id | tonumber)) | .relatives'
+| jq -r --arg id "$HERO_ID" '.[] | select(.id == ($id | tonumber)) | .relatives' \
+| sed 's/; Jackie Shorr.*$//'
