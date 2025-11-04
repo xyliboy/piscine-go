4e7d1bb7abc0c20db11930deb4105009e36a0a00
gxylinas <woodengeorgio@gmail.com.com>
2025-11-04 19:29:19 +0200

    fix2

--- teacher.sh
+++ teacher.sh
@@ -1,1 +1,4 @@
-export INTERVIEW_ID=$(head -n 179 streets/Buckingham_Palace | cat -d '#' )
+export INTERVIEW_ID=$(head -n 179 streets/Buckingham_Place | tail -n 1 | cut -d'#' -f2)
+echo "$INTERVIEW_ID"
+cat "./interviews/interview-$INTERVIEW_ID"
+echo "$MAIN_SUSPECT"
