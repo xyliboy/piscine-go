3796ebb60a9ac0e4931ddcc9fa96dcb3fddf8166
gxylinas <woodengeorgio@gmail.com.com>
2025-11-04 16:59:17 +0200

    teacher.sh

---
+++ teacher.sh
@@ -0,0 +1,5 @@
+INTERVIEW_ID=$(grep -R "CLUE" mystery | grep -oE 'interview_[0-9]+' | grep -oE '[0-9]+')
+echo "$INTERVIEW_ID"
+cat "mystery/interviews/interview_$INTERVIEW_ID"  
+echo "$MAIN_SUSPECT"
+
