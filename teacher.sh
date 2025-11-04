3c26cca986bab092e4497b34d72ed150c60fc301
gxylinas <woodengeorgio@gmail.com.com>
2025-11-04 19:23:45 +0200

    arxidia2

--- teacher.sh
+++ teacher.sh
@@ -1,4 +1,1 @@
-export INTERVIEW_ID=$(head -n 179 streets/Buckingham_Place | tail -n 1 | cut -d'#' -f2)
-echo "$INTERVIEW_ID" 
-cat interviews/interview-$INTERVIEW_ID
-echo "$MAIN_SUSPECT"
+export INTERVIEW_ID=$(head -n 179 streets/Buckingham_Palace | cat -d '#' )
