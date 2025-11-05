80a4cc4535f32ea8fec15939fe73d967e7bd414e
gxylinas <woodengeorgio@gmail.com>
2025-11-05 15:23:37 +0200

    isnegative

--- isnegative/main.go
+++ isnegative/main.go
@@ -3,9 +3,10 @@ package isnegative
 import "github.com/01-edu/z01"
 
 func isnegative(n int) {
-	if n<0 {z01.PrintRune ('T')
-	}
-	else {z01.PrintRune ('F')
+	if n < 0 {
+		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
 }
