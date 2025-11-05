c6c22f41ed91eaeda6bb04a5bdc68bb5d394164d
gxylinas <woodengeorgio@gmail.com>
2025-11-05 19:56:06 +0200

    printit

--- printcomb2.go
+++ printcomb2.go
@@ -3,16 +3,19 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for a := 0; a <= 98; a++; {
-		for b :=a+1; b <= 99; b++; {
-			z01.PrintRune(a + '0')
-			z01.PrintRune('')
-			Z01.PrintRune(b + '0')
-			if a =/ {
-				Z01.PrintRune(',')
-			}
+	for a := 0; a <= 98; a++ {
+		for b := a + 1; b <= 99; b++ {
+			z01.PrintRune(rune(a/10 + '0'))
+			z01.PrintRune(rune(a%10 + '0'))
+			z01.PrintRune(' ')
+			z01.PrintRune(rune(b/10 + '0'))
+			z01.PrintRune(rune(b%10 + '0'))
 
+			if !(a == 98 && b == 99) {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
 		}
 	}
-	Z01.PrintRune('\n')
+	z01.PrintRune('\n')
 }
