d536dd81e4a1010e45ece7f93752f25f549dbbe8
gxylinas <woodengeorgio@gmail.com>
2025-11-06 20:57:16 +0200

    strrev

---
+++ strrev.go
@@ -0,0 +1,11 @@
+package piscine
+
+func StrRev(s string) string {
+	runes := []rune(s)
+	length := len(runes)
+
+	for i := 0; i < length/2; i++ {
+		runes[i], runes[length-1-i] = runes[length-1-i], runes[1]
+	}
+	return s
+}
