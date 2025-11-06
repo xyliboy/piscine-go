548b1685cf55049f3dc5136120c36856b1575834
gxylinas <woodengeorgio@gmail.com>
2025-11-06 23:08:24 +0200

    sort

---
+++ sortintegertable.go
@@ -0,0 +1,15 @@
+package piscine
+
+func SortIntegerTable(table []int) {
+	n := len(table)
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-1; j++ {
+			if table[j] > table[j+1] {
+				temp := table[j]
+				table[j] = table[j+1]
+				table[j+1] = temp
+			}
+		}
+	}
+}
