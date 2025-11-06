9df9c143574b4e8b3b38603d89b5bd6407a59366
gxylinas <woodengeorgio@gmail.com>
2025-11-06 21:03:06 +0200

    strrev

--- strrev.go
+++ strrev.go
@@ -5,7 +5,7 @@ func StrRev(s string) string {
 	length := len(runes)
 
 	for i := 0; i < length/2; i++ {
-		runes[i], runes[length-1-i] = runes[length-1-i], runes[1]
+		runes[i] = runes[length-1-i]
 	}
 	return string(runes)
 }
