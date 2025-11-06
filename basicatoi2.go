8bd613b30611e46675b4513ed09e71c59e95d5b9
gxylinas <woodengeorgio@gmail.com>
2025-11-06 22:47:33 +0200

    basicatoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -3,6 +3,9 @@ package piscine
 func BasicAtoi2(s string) int {
 	result := 0
 	for _, char := range s {
+		if char < '0' || char > '9' {
+			return 0
+		}
 		result = result*10 + int(char-'0')
 	}
 	return result
