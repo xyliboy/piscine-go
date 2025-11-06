29bf9f4b59523be4ef61876d8f37380e843502f4
gxylinas <woodengeorgio@gmail.com>
2025-11-06 18:20:29 +0200

    printstr

--- printstr.go
+++ printstr.go
@@ -2,17 +2,7 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-// func PrintStr(a *string) {
-// 	*a= *a + "."
-// 	b= *a
-// 	b := []byte(b)
-// 	c=0
-// 	for b/="."
-// 	z01.PrintRune (byte(b))
-// }
-
-func PrintStr(a *string) {
-	s := *a
+func PrintStr(s string) {
 	for i := 0; i < len(s); i++ {
 		z01.PrintRune(rune(s[i]))
 	}
