93585b5f6daab6324f2a7b20aeb9507fd2e5873c
Giorgos Xylinas <xyliboy@192.168.1.11>
2025-11-10 23:47:21 +0200

    itpow

---
+++ iterativepower.go
@@ -0,0 +1,23 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	res := nb
+	if power < 0 {
+		return 0
+	}
+
+	if power == 0 {
+		return 1
+	}
+
+	if power == 1 {
+		return nb
+	}
+
+	if power > 1 {
+		for i := 2; i == power; i++ {
+			res = res * nb
+		}
+	}
+	return res
+}
