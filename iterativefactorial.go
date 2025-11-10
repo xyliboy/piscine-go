e57f9a5ae382f4268958a9f9d4b2dcf5cc33d667
gxylinas <woodengeorgio@gmail.com>
2025-11-10 16:53:03 +0200

    Iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,10 +4,11 @@ func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	}
-
-	sum := 0
+	sum := 1
+	if nb == 0 || nb == 1 {
+		return 0
+	}
 	if nb > 1 && nb <= 20 {
-		sum = 1
 		for i := 2; i <= nb; i++ {
 			sum = sum * i
 		}
