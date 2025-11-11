db7bc24a67c5eff8aa2337154b03dd6bc60baa57
gxylinas <woodengeorgio@gmail.com>
2025-11-11 18:22:04 +0200

    concat

---
+++ concat.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Concat(str1 string, str2 string) string {
+	if str1 != "" && str2 != "" {
+		return str1 + str2
+	}
+	if str1 == "" {
+		return str2
+	}
+	if str2 == "" {
+		return str1
+	}
+}
