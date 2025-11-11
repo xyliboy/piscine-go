a294b023ad57184937c362722739cdf9862b9a95
Giorgos Xylinas <xyliboy@192.168.1.11>
2025-11-11 02:30:48 +0200

    FNP

--- findnextprime.go
+++ findnextprime.go
@@ -5,7 +5,18 @@ func FindNextPrime(nb int) int {
 		return 2
 	}
 	for {
-		if IsPrime(nb) {
+		isPrime := true
+		if nb%2 == 0 && nb != 2 {
+			isPrime = false
+		} else {
+			for i := 3; i*i <= nb; i += 2 {
+				if nb%i == 0 {
+					isPrime = false
+					break
+				}
+			}
+		}
+		if isPrime {
 			return nb
 		}
 		nb++
