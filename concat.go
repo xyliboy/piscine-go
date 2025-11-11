2d9988e65349f8d2c6cc6019b30b67d6ab37cabf
gxylinas <woodengeorgio@gmail.com>
2025-11-11 18:22:46 +0200

    concat

--- concat.go
+++ concat.go
@@ -1,13 +1,11 @@
 package piscine
 
 func Concat(str1 string, str2 string) string {
-	if str1 != "" && str2 != "" {
-		return str1 + str2
-	}
 	if str1 == "" {
 		return str2
 	}
 	if str2 == "" {
 		return str1
 	}
+	return str1 + str2
 }
