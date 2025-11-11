6b3378239e336425265adea71e0a08c6a08744fb
gxylinas <woodengeorgio@gmail.com>
2025-11-11 20:08:53 +0200

    tolow

--- tolower.go
+++ tolower.go
@@ -3,7 +3,7 @@ package piscine
 func ToLower(s string) string {
 	result := ""
 	for _, r := range s {
-		if r >= 'a' && r <= 'z' {
+		if r >= 'A' && r <= 'Z' {
 			result += string(r + 32)
 		} else {
 			result += string(r)
