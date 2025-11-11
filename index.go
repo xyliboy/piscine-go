521d37bcbd4a6714622c9082bf1ed44b7363d1bd
gxylinas <woodengeorgio@gmail.com>
2025-11-11 17:55:29 +0200

    inde

---
+++ index.go
@@ -0,0 +1,26 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	runess := []rune(s)
+	runest := []rune(toFind)
+	lengtht := len(toFind)
+	i := 0
+	j := 0
+	for range s {
+		if runess[i] == runest[0] {
+			for range toFind {
+				a := i
+				if runess[a] == runest[j] {
+					j++
+					a++
+				}
+			}
+			if j == lengtht {
+				return i
+			}
+			j = 0
+		}
+		i++
+	}
+	return 0
+}
