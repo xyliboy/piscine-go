01edaeac412241a09f81377309c5d3556efd44b7
gxylinas <woodengeorgio@gmail.com>
2025-11-11 14:03:07 +0200

    nrune

--- nrune.go
+++ nrune.go
@@ -1,13 +1,10 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	i := -1
 	runes := []rune(s)
-	for range s {
-		i++
+	length := len(runes)
+	if length < n {
+		return 0
 	}
-	if n == i && i >= 0 {
-		return runes[n]
-	}
-	return 0
+	return runes[n]
 }
