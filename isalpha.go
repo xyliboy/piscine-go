d4e23d74a4eeb7207a91f9a8a533ad559d01e2d3
gxylinas <woodengeorgio@gmail.com>
2025-11-11 19:15:21 +0200

    isalp

---
+++ isalpha.go
@@ -0,0 +1,18 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	runes := []rune(s)
+	length := len(runes)
+	i := 0
+	j := 0
+	for range s {
+		if s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' || s[i] <= '0' || s[i] > '0' {
+			j++
+		}
+		i++
+	}
+	if length == j {
+		return true
+	}
+	return false
+}
