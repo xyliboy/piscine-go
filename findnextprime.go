e0851a09d0c830e502f49d22eb898d8dc1f2f214
Giorgos Xylinas <xyliboy@192.168.1.11>
2025-11-11 02:28:01 +0200

    FNP

--- findnextprime.go
+++ findnextprime.go
@@ -1,19 +1,5 @@
 package piscine
 
-func IsPrime(nb int) bool {
-	if nb <= 1 {
-		return false
-	}
-	i := 2
-	for i < nb {
-		if nb%i == 0 {
-			return false
-		}
-		i++
-	}
-	return true
-}
-
 func FindNextPrime(nb int) int {
 	if nb < 2 {
 		return 2
