670565fc354df9c6c8ae93dc79cb3952b19cc423
gxylinas <woodengeorgio@gmail.com>
2025-11-12 18:10:30 +0200

    main

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"os" // για να πάρουμε τα arguments
+	"os" // για να πάρουμε το path του προγράμματος
 
-	"github.com/01-edu/z01" // για εκτύπωση χαρακτήρα-χαρακτήρα
+	"github.com/01-edu/z01" // για PrintRune
 )
 
 func main() {
-	name := os.Args[0] // το όνομα του προγράμματος (πρώτο argument πάντα)
+	path := os.Args[0]    // π.χ. "/home/user/go/choumi" ή "./choumi"
+	runes := []rune(path) // μετατροπή σε runes για σωστή επεξεργασία χαρακτήρων
 
-	for _, r := range name { // για κάθε χαρακτήρα (rune) στο όνομα
-		z01.PrintRune(r) // τύπωσε τον χαρακτήρα
+	start := 0                             // index από όπου ξεκινάει το όνομα
+	for i := len(runes) - 1; i >= 0; i-- { // πάμε από το τέλος προς τα πίσω
+		if runes[i] == '/' { // αν βρούμε "/", άρα εκεί τελειώνει το path
+			start = i + 1 // το όνομα ξεκινάει μετά από αυτό
+			break
+		}
+	}
+
+	for j := start; j < len(runes); j++ { // τυπώνουμε από start μέχρι τέλος
+		z01.PrintRune(runes[j]) // εκτύπωση κάθε χαρακτήρα
 	}
 	z01.PrintRune('\n') // αλλαγή γραμμής στο τέλος
 }
