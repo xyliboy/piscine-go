026ef19b95918f789f98f2578926a384f4ab80af
gxylinas <woodengeorgio@gmail.com>
2025-11-13 17:54:25 +0200

    appran

--- appendrange.go
+++ appendrange.go
@@ -4,7 +4,7 @@ func AppendRange(min, max int) []int {
 	nums := []int{}
 	dif := max - min
 	if dif > 0 {
-		for i := 0; i < dif; i++ {
+		for i := 0; i <= dif-1; i++ {
 			nums = append(nums, min+i)
 		}
 	}
