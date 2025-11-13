bdb95852acfccb8ec0bde32b821f9750cdff9d22
gxylinas <woodengeorgio@gmail.com>
2025-11-13 17:27:26 +0200

    appran

---
+++ appendrange.go
@@ -0,0 +1,12 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	nums := []int{}
+	dif := max - min
+	if dif > 0 {
+		for i := 0; i <= dif; i++ {
+			nums = append(nums, min+i)
+		}
+	}
+	return nums
+}
