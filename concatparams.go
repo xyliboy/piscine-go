68f0a9859481c05b08a8cc08dd85b28e6f4dd6eb
gxylinas <woodengeorgio@gmail.com>
2025-11-13 21:27:06 +0200

    concat

--- concatparams.go
+++ concatparams.go
@@ -4,9 +4,9 @@ func ConcatParams(args []string) string {
 	var leksi string
 	for v := range args {
 		if v < len(args)-1 {
-			leksi = leksi + args[v]
-		} else {
 			leksi = leksi + args[v] + "\n"
+		} else {
+			leksi = leksi + args[v]
 		}
 	}
 	return leksi
