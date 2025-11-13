1a50ba2687c0bc6d1a6df6d2c7d0093621d67ffd
gxylinas <woodengeorgio@gmail.com>
2025-11-13 19:01:35 +0200

    makran

--- makerange.go
+++ makerange.go
@@ -4,10 +4,9 @@ func MakeRange(min, max int) []int {
 	var nums []int
 	dif := max - min
 	if dif > 0 {
+		nums = make([]int, dif)
 		for i := 0; i <= dif-1; i++ {
-			nums = make([]int, dif)
 			nums[i] = min + i
-
 		}
 	}
 	return nums
