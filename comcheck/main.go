e17a594bf9d14292b55bfd2ed3079bda638a7444
gxylinas <woodengeorgio@gmail.com>
2025-11-21 01:18:37 +0200

    met

--- comcheck/main.go
+++ comcheck/main.go
@@ -7,8 +7,8 @@ import (
 
 func ComCheck() {
 	leksi := os.Args[1:]
-	for _, i := range leksi {
-		if i == "01" || i == "galaxy" || i == "galaxy 01" {
+	for _, j := range leksi {
+		if j == "01" || j == "galaxy" || j == "galaxy 01" {
 			fmt.Println("Alert!!!")
 			break
 		}
