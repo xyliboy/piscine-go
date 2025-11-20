e01cbce489cb189ffb63a52bc2a9c5c54983b4f9
gxylinas <woodengeorgio@gmail.com>
2025-11-20 19:12:23 +0200

    met

--- jumpover.go
+++ jumpover.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func JumpOver(str string) string {
 	runes := []rune(str)
 	if len(runes) > 2 {
-		for i := 2; i <= len(runes); i += 3 {
+		for i := 2; i < len(runes); i += 3 {
 			z01.PrintRune(runes[i])
 		}
 	}
