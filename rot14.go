a14dcbcb3209641197b2ad8004df36b5988aae05
gxylinas <woodengeorgio@gmail.com>
2025-11-20 10:49:24 +0200

    chalet

--- rot14.go
+++ rot14.go
@@ -5,7 +5,8 @@ func Rot14(s string) string {
 	for i, ch := range runes {
 		if ch >= 'a' && ch <= 'z' {
 			runes[i] = 'a' + (ch-'a'+14)%26
-		} else if ch >= 'A' && ch <= 'Z' {
+		}
+		if ch >= 'A' && ch <= 'Z' {
 			runes[i] = 'A' + (ch-'A'+14)%26
 		}
 	}
