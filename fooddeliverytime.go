d0ba2d69e1acab00c96c7e83a1d7322da7408cd5
gxylinas <woodengeorgio@gmail.com>
2025-11-20 13:34:08 +0200

    met

---
+++ fooddeliverytime.go
@@ -0,0 +1,31 @@
+package piscine
+
+type food struct {
+	preptime int
+}
+
+func FoodDeliveryTime(order string) int {
+	faghto := food{}
+	fai := 0
+	count := 0
+	for i := 0; i < len(order); i++ {
+		if order[i] != ' ' {
+			count++
+		}
+		if order[i] == ' ' {
+			if count == 6 {
+				fai = fai + 15
+			}
+			if count == 5 {
+				fai = fai + 10
+			}
+			if count == 7 {
+				fai = fai + 12
+			}
+			count = 0
+		}
+
+	}
+	faghto.preptime = fai
+	return faghto.preptime
+}
