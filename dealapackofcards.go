514d85bd1440187d2845a9e13822b22409447604
gxylinas <woodengeorgio@gmail.com>
2025-11-20 18:29:29 +0200

    met

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -1,49 +1,20 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
-func printString(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
-	}
-}
-
-func printInt(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-	digits := []rune{}
-	for n > 0 {
-		digits = append(digits, rune(n%10+'0'))
-		n /= 10
-	}
-	for i := len(digits) - 1; i >= 0; i-- {
-		z01.PrintRune(digits[i])
-	}
-}
+import "fmt"
 
 func DealAPackOfCards(deck []int) {
 	player := 1
+	index := 0
 
-	for i := 0; i < 12; i += 3 {
-		// Player X:
-		printString("Player ")
-		printInt(player)
-		printString(": [")
+	for player <= 4 {
+		fmt.Printf("Player %d: [%d, %d, %d]\n",
+			player,
+			deck[index],
+			deck[index+1],
+			deck[index+2],
+		)
 
-		// Τα 3 χαρτιά
-		printInt(deck[i])
-		printString(", ")
-		printInt(deck[i+1])
-		printString(", ")
-		printInt(deck[i+2])
-
-		printString("]\n")
+		index += 3
 		player++
 	}
 }
