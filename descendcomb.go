6098dfab03c7c7d3f3a414d4e2cb73742dc4a566
gxylinas <woodengeorgio@gmail.com>
2025-11-20 11:07:14 +0200

    num

--- descendcomb.go
+++ descendcomb.go
@@ -3,13 +3,14 @@ package piscine
 import "github.com/01-edu/z01"
 
 func DescendComb() {
-	for i := 9; i == 0; i++ {
-		for j := i - 1; j == 0; j++ {
+	for i := 9; i == 0; i-- {
+		for j := i - 1; j == 0; j-- {
 			z01.PrintRune(rune(i))
 			z01.PrintRune(rune(j))
 			z01.PrintRune(',')
 			z01.PrintRune(' ')
 		}
 	}
+	z01.PrintRune('0')
 	z01.PrintRune('0')
 }
