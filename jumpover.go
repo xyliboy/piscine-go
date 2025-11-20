31d7036af5bd36a9c3d4b45fa899cc35929c49f2
gxylinas <woodengeorgio@gmail.com>
2025-11-20 19:17:07 +0200

    met

--- jumpover.go
+++ jumpover.go
@@ -1,13 +1,13 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func JumpOver(str string) string {
+	word := []rune{}
 	runes := []rune(str)
 	if len(runes) > 2 {
 		for i := 2; i < len(runes); i += 3 {
-			z01.PrintRune(runes[i])
+			word = append(word, runes[i])
 		}
 	}
-	return "\n"
+	word = append(word, '\n')
+	return string(word)
 }
