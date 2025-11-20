2e5e5356c355a251d0d3038cacd0850118adceb7
gxylinas <woodengeorgio@gmail.com>
2025-11-20 12:34:08 +0200

    met

--- unmatch.go
+++ unmatch.go
@@ -1,45 +1,45 @@
-// package piscine
-
-// func Unmatch(a []int) int {
-// 	count := 0
-// 	met := 0
-// 	for i := range a {
-// 		for j := range a {
-// 			if i != j {
-// 				if a[i] == a[j] {
-// 					count = 0
-// 				} else {
-// 					count++
-// 				}
-// 			}
-// 		}
-// 		if count != 0 {
-// 			met = i
-// 			return a[met]
-// 		}
-
-// 	}
-
-// 	return -1
-// }
-
 package piscine
 
 func Unmatch(a []int) int {
-	for i, num := range a {
-		count := 0
+	count := 1
+	met := 0
+	for i := range a {
 		for j := range a {
 			if i != j {
 				if a[i] == a[j] {
+					count = 0
+				} else {
 					count++
 				}
 			}
 		}
-		if count%2 != 0 {
-			return num
+		if count != 0 {
+			met = i
+			return a[met]
 		}
 
 	}
 
 	return -1
 }
+
+// package piscine
+
+// func Unmatch(a []int) int {
+// 	for i, num := range a {
+// 		count := 1
+// 		for j := range a {
+// 			if i != j {
+// 				if a[i] == a[j] {
+// 					count++
+// 				}
+// 			}
+// 		}
+// 		if count%2 != 0 {
+// 			return num
+// 		}
+
+// 	}
+
+// 	return -1
+// }
