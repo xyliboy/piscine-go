5665dd39e54c5a323dee7a10191795838451fd95
gxylinas <woodengeorgio@gmail.com>
2025-11-20 16:50:36 +0200

    met

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -3,21 +3,19 @@ package piscine
 func ShoppingSummaryCounter(str string) map[string]int {
 	lista := map[string]int{}
 	word := ""
-
 	for _, ch := range str {
 		if (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') {
 			word += string(ch)
-		} else {
-			if word != "" {
-				lista[word]++
-				word = ""
-			}
+		} else if word != "" {
+			lista[word]++
+			word = ""
+		} else if word == " " {
+			lista[""]++
+			word = ""
 		}
 	}
-
 	if word != "" {
 		lista[word]++
 	}
-
 	return lista
 }
