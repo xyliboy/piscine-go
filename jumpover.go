44bd03bf47500d9140abbd430c5d019e00857c71
gxylinas <woodengeorgio@gmail.com>
2025-11-20 19:09:35 +0200

    met

--- jumpover.go
+++ jumpover.go
@@ -3,12 +3,11 @@ package piscine
 import "github.com/01-edu/z01"
 
 func JumpOver(str string) string {
-	if len(str) < 3 || str == "" {
-		return "\n"
-	} else {
+	if len(str) > 2 {
 		runes := []rune(str)
 		for i := 2; i <= len(str); i += 3 {
 			z01.PrintRune(runes[i])
 		}
 	}
+	return "\n"
 }
