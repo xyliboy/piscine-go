b24bf4271d5b043465e53328adb4a8dec2066c3c
gxylinas <woodengeorgio@gmail.com>
2025-11-20 12:12:19 +0200

    met

--- unmatch.go
+++ unmatch.go
@@ -3,8 +3,8 @@ package piscine
 func Unmatch(a []int) int {
 	count := 0
 	met := ' '
-	for i := range len(a) {
-		for j := range len(a) {
+	for i := range a {
+		for j := range a {
 			if i != j {
 				if a[i] == a[j] {
 					count = 0
