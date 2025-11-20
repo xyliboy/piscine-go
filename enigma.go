a288d934196945971bc338b69658c2e02966610e
gxylinas <woodengeorgio@gmail.com>
2025-11-20 22:14:42 +0200

    met

---
+++ enigma.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	ena := ***a
+	dyo := *b
+	tria := *******c
+	tes := ****d
+	***a = dyo
+	*b = tria
+	*******c = tes
+	****d = ena
+}
