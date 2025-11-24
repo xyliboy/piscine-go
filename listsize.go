c78da8b276fea83fc3e71c4a73aecd0635e15565
gxylinas <woodengeorgio@gmail.com>
2025-11-24 16:11:50 +0200

    size

---
+++ listsize.go
@@ -0,0 +1,24 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListSize(l *List) int {
+	num := 0
+	if l.Tail == nil {
+		return num
+	}
+	num++
+	for l.Head != nil {
+		l.Head = l.Head.Next
+		num++
+	}
+	return num
+}
