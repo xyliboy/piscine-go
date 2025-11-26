01ac649a9158ed580f4a9785e8d88f6054a7c411
gxylinas <woodengeorgio@gmail.com>
2025-11-26 17:18:40 +0200

    lista

---
+++ listsort.go
@@ -0,0 +1,30 @@
+package piscine
+
+type NodeI struct {
+	Data int
+	Next *NodeI
+}
+
+func ListSort(l *NodeI) *NodeI {
+	if l == nil {
+		return nil
+	}
+	count := 0
+	node := l
+	for node != nil {
+		count++
+		node = node.Next
+	}
+
+	for i := 0; i < count; i++ {
+		curr := l
+		for curr.Next != nil {
+			if curr.Data > curr.Next.Data {
+				curr.Data, curr.Next.Data = curr.Next.Data, curr.Data
+			}
+			curr = curr.Next
+		}
+	}
+
+	return l
+}
