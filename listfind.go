ef42525d9f5ca18b1698a22eb03408e2243917df
gxylinas <woodengeorgio@gmail.com>
2025-11-26 13:51:49 +0200

    lista

---
+++ listfind.go
@@ -0,0 +1,26 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func CompStr(a, b interface{}) bool {
+	return a == b
+}
+
+func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
+	node := l.Head    // ξεκινάμε από το πρώτο node
+	for node != nil { // όσο υπάρχουν nodes στην λίστα
+		if comp(node.Data, ref) { // αν comp λέει ότι είναι ίσα
+			return &node.Data // επιστρέφουμε pointer στο Data
+		}
+		node = node.Next // πάμε στο επόμενο
+	}
+	return nil
+}
