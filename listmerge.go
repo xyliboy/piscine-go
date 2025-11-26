83cdbc6b24c20924dd84d8588f7deb91d00fdc32
gxylinas <woodengeorgio@gmail.com>
2025-11-26 15:51:02 +0200

    lista

---
+++ listmerge.go
@@ -0,0 +1,28 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListMerge(l1 *List, l2 *List) {
+	if l2.Head == nil {
+		return
+	}
+
+	// Αν η l1 είναι άδεια → απλά γίνει ίδια με l2
+	if l1.Head == nil {
+		l1.Head = l2.Head
+		l1.Tail = l2.Tail
+		return
+	}
+
+	// Και οι δύο λίστες έχουν στοιχεία → κάνε merge
+	l1.Tail.Next = l2.Head
+	l1.Tail = l2.Tail
+}
